06cdd9b8dbc45a57e04b02e654216d9862836c3a
chenchen <chenchen9@iflytek.com>
2015-05-13 16:56:40 +0800

    x

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
       name="wtp",
-        version="1.0.0",
+        version="1.0.1",
         packages=find_packages(),
         zip_safe=False,
 
